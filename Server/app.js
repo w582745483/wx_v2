57b3703e0b26068230e7cc049a9b72deec241ca9
wjy王君瑶 <junyao.wang@ctrip.com>
2019-10-29 14:25:16 +0800

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // 处理跨域
 app.all('*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')//http://ruige.wicp.vip
+  res.header('Access-Control-Allow-Origin', 'http://47.103.112.148:33333')//http://ruige.wicp.vip //http://47.103.112.148:33333
   res.header(
       'Access-Control-Allow-Headers',
       'Content-Type,Content-Length, Authorization, Accept,X-Requested-With,Token'
