93146545875c9a518fb87e25629af8cef8d386ff
wjy王君瑶 <junyao.wang@ctrip.com>
2019-12-10 09:06:02 +0800

    up

--- Server/check.js
+++ Server/check.js
@@ -4,7 +4,7 @@ let process = require('child_process');
 let ChildProcess  = process.fork('./bin/www'); 
 ChildProcess.on('exit',function (code) {
     console.log('process exits + '+code);
-    //fs.appendFileSync('./log/log.txt',`${new Date()} 线程退出`+code);
+    fs.appendFileSync('./log/log.txt',`${new Date()} 线程退出`+code);
     if(code !== 0){
         console.log('服务退出，正在重启。。。')
         process.fork('./check.js');
